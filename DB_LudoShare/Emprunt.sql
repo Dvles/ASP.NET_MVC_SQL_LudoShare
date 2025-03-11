739e859e35e65eab2a0464762e2569f5ba3a6021
Dvles <132080646+Dvles@users.noreply.github.com>
2025-03-11 11:55:55 +0100

    Relations: ajout de contraintes d'intégrité sur la table Emprunt ++
    Ajout de la colonne Etat pour enregistrer l’état du jeu au moment de l’emprunt, Ajout des clés étrangères :
    - FK_Emprunt_UPJ → Associe l'emprunt à un jeu possédé
    - FK_Emprunt_Prete → Vérifie que le prêteur est bien un utilisateur
    
    Ajout de la contrainte CK_Emprunt_Etat pour empêcher l’emprunt d’un jeu en état "Incomplet"
    
    - FK_Emprunt_Emprunte → Vérifie que l’emprunteur est bien un utilisateur

--- DB_LudoShare/Emprunt.sql
+++ DB_LudoShare/Emprunt.sql
@@ -1,19 +1,17 @@
 ﻿CREATE TABLE [dbo].[Emprunt]
 (
     [Emprunt_Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-    [Jeux_Id] UNIQUEIDENTIFIER NOT NULL, 
+    [UPJ_Id] UNIQUEIDENTIFIER NOT NULL,
     [Preteur_Id] UNIQUEIDENTIFIER NOT NULL, 
     [Emprunteur_Id] UNIQUEIDENTIFIER NOT NULL,
     [DateEmprunt] DATETIME2 NOT NULL DEFAULT GETDATE(),
-    [DateRetour] DATETIME2 NULL, 
+    [DateRetour] DATETIME2 NULL,
     [EvaluationPreteur] TINYINT CHECK (EvaluationPreteur BETWEEN 0 AND 5),
     [EvaluationEmprunteur] TINYINT CHECK (EvaluationEmprunteur BETWEEN 0 AND 5),
+    [Etat] NVARCHAR(50) NOT NULL,
 
-    CONSTRAINT FK_Emprunt_Jeu FOREIGN KEY ([Jeux_Id]) REFERENCES [dbo].[Jeux]([Jeux_Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_Emprunt_UPJ FOREIGN KEY ([UPJ_Id]) REFERENCES [dbo].[Utilisateur_Posseder_Jeux]([UPJ_Id]) ON DELETE CASCADE,
     CONSTRAINT FK_Emprunt_Prete FOREIGN KEY ([Preteur_Id]) REFERENCES [dbo].[Utilisateur]([Utilisateur_Id]) ON DELETE CASCADE,
     CONSTRAINT FK_Emprunt_Emprunte FOREIGN KEY ([Emprunteur_Id]) REFERENCES [dbo].[Utilisateur]([Utilisateur_Id]) ON DELETE CASCADE,
-
-
+    CONSTRAINT CK_Emprunt_Etat CHECK ([Etat] IN ('Neuf', 'Abimé'))
 );
-
-
