dd9ef2047eecb78d19b04dbbbd0ca8f593a00bef
Dvles <132080646+Dvles@users.noreply.github.com>
2025-03-11 10:35:51 +0100

    Creation table Tag & Emprunt (Sql script)

---
+++ DB_LudoShare/Emprunt.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Emprunt]
+(
+	[DateEmprunt] DATETIME2 NOT NULL,
+	[DateRetour] DATETIME2 NOT NULL,
+	[EvaluationPreteur] TINYINT,
+	[EvaluationEmprunteur] TINYINT,
+)
---
+++ DB_LudoShare/Tag.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Tag]
+(
+	[Tag_Id] INT NOT NULL PRIMARY KEY,
+	[Tag]   VARBINARY (64)   NOT NULL,
+
+)
