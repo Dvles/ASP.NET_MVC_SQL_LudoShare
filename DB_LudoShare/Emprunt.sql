fd200b41c18210b74d1cf22bc9cb2706e8b9e72a
Dvles <132080646+Dvles@users.noreply.github.com>
2025-03-11 11:46:53 +0100

    Relations: ajout de la contrainte de clé étrangère (Preteurs/Emprunteurs) dans table Emprunt

--- DB_LudoShare/Emprunt.sql
+++ DB_LudoShare/Emprunt.sql
@@ -1,13 +1,19 @@
 ﻿CREATE TABLE [dbo].[Emprunt]
 (
-	[Emprunt_Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
-	[Jeux_Id] UNIQUEIDENTIFIER NOT NULL, 
-	[DateEmprunt] DATETIME2 NOT NULL,
-	[DateRetour] DATETIME2 NOT NULL,
-	[EvaluationPreteur] TINYINT,
-	[EvaluationEmprunteur] TINYINT,
-	CONSTRAINT FK_Emprunt_Jeu FOREIGN KEY ([Jeux_Id]) REFERENCES [dbo].[Jeux]([Jeux_Id]) ON DELETE CASCADE,
+    [Emprunt_Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID() PRIMARY KEY,
+    [Jeux_Id] UNIQUEIDENTIFIER NOT NULL, 
+    [Preteur_Id] UNIQUEIDENTIFIER NOT NULL, 
+    [Emprunteur_Id] UNIQUEIDENTIFIER NOT NULL,
+    [DateEmprunt] DATETIME2 NOT NULL DEFAULT GETDATE(),
+    [DateRetour] DATETIME2 NULL, 
+    [EvaluationPreteur] TINYINT CHECK (EvaluationPreteur BETWEEN 0 AND 5),
+    [EvaluationEmprunteur] TINYINT CHECK (EvaluationEmprunteur BETWEEN 0 AND 5),
+
+    CONSTRAINT FK_Emprunt_Jeu FOREIGN KEY ([Jeux_Id]) REFERENCES [dbo].[Jeux]([Jeux_Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_Emprunt_Prete FOREIGN KEY ([Preteur_Id]) REFERENCES [dbo].[Utilisateur]([Utilisateur_Id]) ON DELETE CASCADE,
+    CONSTRAINT FK_Emprunt_Emprunte FOREIGN KEY ([Emprunteur_Id]) REFERENCES [dbo].[Utilisateur]([Utilisateur_Id]) ON DELETE CASCADE,
 
 
-)
+);
+
 
