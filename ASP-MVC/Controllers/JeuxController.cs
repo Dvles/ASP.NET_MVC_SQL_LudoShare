43c04c2e61ce7e5a78e889f1f6920fe826f62294
Dvles <132080646+Dvles@users.noreply.github.com>
2025-03-13 14:54:44 +0100

    MVC: JeuxController: ajout détail jeu + bouton "Voir plus" sur chaque jeu (home/index)

---
+++ ASP-MVC/Controllers/JeuxController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BLL.Services;
+using BLL.Entities;
+
+namespace ASP_MVC.Controllers
+{
+	public class JeuxController : Controller
+	{
+		private readonly JeuxService _jeuxService;
+
+		public JeuxController(JeuxService jeuxService)
+		{
+			_jeuxService = jeuxService;
+		}
+
+		public IActionResult Details(Guid id)
+		{
+			var jeu = _jeuxService.GetById(id);
+			if (jeu == null)
+			{
+				return NotFound();
+			}
+			return View(jeu);
+		}
+	}
+}
